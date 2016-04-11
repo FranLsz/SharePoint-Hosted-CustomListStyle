3b354df5d15fb01ab36b8256838193453bdb629b
FranLsz <franlsz95develop@gmail.com>
2016-04-11 13:20:51 +0200

    Subida

---
+++ SpHostedCustomListStyle/Features/Feature1/Feature1.feature
@@ -0,0 +1,15 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="2abae15a-99ca-4da1-a1cc-3ea068028488" description="SharePoint Add-in Feature" featureId="2abae15a-99ca-4da1-a1cc-3ea068028488" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SpHostedCustomListStyle Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="8c07ea3d-5769-45a0-8ad6-39d1d5924692" />
+    <projectItemReference itemId="8b9bad0e-3754-4279-b7af-a268b788bccc" />
+    <projectItemReference itemId="3099f513-d6b1-4db6-9942-eb310d07989e" />
+    <projectItemReference itemId="8228478b-94ae-4983-838e-4a82bac72f85" />
+    <projectItemReference itemId="8b20fe45-905d-4921-b7ce-9a7fbfc9ef6b" />
+    <projectItemReference itemId="ba50e723-9058-4fae-bc49-84b08be08c4e" />
+    <projectItemReference itemId="fdee7fee-9b99-4ec4-b574-242039e65f4c" />
+    <projectItemReference itemId="0a993a72-f797-4854-bbc9-da0f6463b616" />
+    <projectItemReference itemId="3f87d6fc-7817-4f46-90bd-5f393aef21c0" />
+    <projectItemReference itemId="82d862a5-1d87-4744-af42-dbf00a08220b" />
+  </projectItems>
+</feature>
---
+++ SpHostedCustomListStyle/Scripts/customizeview.js
@@ -0,0 +1,69 @@
+﻿window.Cust = window.Cust || {};
+
+window.Cust.accordionItem = {
+
+    customItemHtml: function (ctx) {
+        var accordionItemHtml = "<h3>" + ctx.CurrentItem.Nombre + "</h3>";
+        accordionItemHtml += "<div>" + ctx.CurrentItem.Descripcion + "</div>";
+        return accordionItemHtml;
+
+    }
+
+};
+
+(function () {
+
+    var appWebUrl = decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
+    var scriptFolder = appWebUrl + "/SiteAssets/";
+    loadcssfile(scriptFolder + "jquery-ui.css", "css");
+    // Initialize the variable that store the objects.
+    var overrideCtx = {};
+    overrideCtx.Templates = {};
+    overrideCtx.Templates.Header = "<div id=\"accordion\">";
+    overrideCtx.Templates.Item = window.Cust.accordionItem.customItemHtml;
+    overrideCtx.Templates.Footer = "</div>";
+    overrideCtx.BaseViewID = 1;
+    overrideCtx.ListTemplateType = 10000;
+
+    // Register the template overrides.
+
+    SPClientTemplates.TemplateManager.RegisterTemplateOverrides(overrideCtx);
+
+})();
+
+$(document).ready(function () {
+
+    $("#accordion").find('#scriptBodyWPQ1').remove();
+    $("#accordion").accordion({ heightStyle: "content" });
+
+});
+
+function loadcssfile(filename, filetype) {
+
+    if (filetype == "css") { //if filename is an external CSS file
+
+        var fileref = document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+
+    }
+
+    if (typeof fileref != "undefined")
+
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+
+}
+
+function getQueryStringParameter(urlParameterKey) {
+
+    var params = document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split('=');
+        if (singleParam[0] == urlParameterKey)
+            return decodeURIComponent(singleParam[1]);
+
+    }
+
+}
